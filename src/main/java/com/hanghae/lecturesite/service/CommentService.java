bffbb0bb370bcb5e94a69ed3d888ce34392ea3a2
alaneelee <alaneelee@gmail.com>
2023-11-16 00:51:00 +0900

    feat: jwt 로직 임시 추가(jwt 구현 완료되면 확인 필요)

--- src/main/java/com/hanghae/lecturesite/service/CommentService.java
+++ src/main/java/com/hanghae/lecturesite/service/CommentService.java
@@ -3,6 +3,7 @@ package com.hanghae.lecturesite.service;
 
 import com.hanghae.lecturesite.entity.Comment;
 import com.hanghae.lecturesite.repository.CommentRepository;
+import io.jsonwebtoken.Claims;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,11 +18,21 @@ public class CommentService {
 
     // 선택한 강의의 선택한 댓글 삭제
     public String deleteComments(Long id, String jwt) {
-        // jwt 로직 작성 필요
+        // jwt 로직 임시 작성(확인 필요)
+        // Claims claims = jwtUtil.getAdminInfoFromToken(jwt);
+        // Long memberId = Long.parseLong(claims.getSubject());
+
         Comment comment = commentRepository.findById(id).orElseThrow(() ->
             new IllegalArgumentException("존재하지 않는 댓글입니다.")
-            );
-        commentRepository.delete(comment);
-        return "댓글이 삭제되었습니다.";
+        );
+
+        // jwt 로직 임시 작성(확인 필요)
+//        if (comment.getMember().getEmail().equals(memberId)) {
+//            commentRepository.delete(comment);
+//            return "댓글이 삭제되었습니다.";
+//        }
+
+        return "본인이 작성한 댓글만 삭제할 수 있습니다.";
+
     }
 }
