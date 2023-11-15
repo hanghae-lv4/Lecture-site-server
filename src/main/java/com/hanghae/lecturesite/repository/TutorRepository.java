6254f05c6fdef7ae5c4686377642280cd9c18022
kangminbeom <alsqja6945@naver.com>
2023-11-15 23:10:47 +0900

    feat: 강사 등록 기능 구현, career 변수 수정, TutorConroller,TutorRepository, TutorResponseDto, TutorRequestDto, TutorRepository, Tutor Service 생성

---
+++ src/main/java/com/hanghae/lecturesite/repository/TutorRepository.java
@@ -0,0 +1,7 @@
+package com.hanghae.lecturesite.repository;
+
+import com.hanghae.lecturesite.entity.Tutor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TutorRepository extends JpaRepository<Tutor, Long> {
+}
